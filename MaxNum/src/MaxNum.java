9a9c32b1d587af9af3bee941ddf2f22d857bb06a
Nanditha Narayana <Nanditha Narayana@DESKTOP-7589HV4>
2021-06-25 06:40:05 -0500

    Initial commit

---
+++ MaxNum/src/MaxNum.java
@@ -0,0 +1,13 @@
+
+public class MaxNum {
+
+	public static void main(String[] args) {
+		int num1=30;
+		int num2=50;
+		double res=java.lang.Math.max(num1,num2);
+		System.out.println(res);
+		
+
+	}
+
+}
